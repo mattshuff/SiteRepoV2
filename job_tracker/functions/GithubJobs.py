fefecdf58042c57a5eb2d6060318df69c0c83789
Matt Shuff <mattshuff1@gmail.com>
2021-02-14 13:24:47 +0000

    doesnt work yet, backing up

--- job_tracker/functions/GithubJobs.py
+++ job_tracker/functions/GithubJobs.py
@@ -4,6 +4,9 @@ from operator import itemgetter
 import json
 import requests
 import json
+import time
+import logging
+logging.basicConfig(level=logging.INFO)
 
 if __name__ == "__main__":
     response = requests.get("https://jobs.github.com/positions.json?&location=uk")
@@ -15,12 +18,28 @@ if __name__ == "__main__":
     final_list = []
     counter = 0
 
-    # Loop over api response, make ID more easily indexed
-    for Entry in sorted_by_date:
-        Entry['id'] = counter
-        counter += 1
-        final_list.append(Entry)
+    """ 
+    Loop over each entry in the sorted list, doing some cleaning up
+        Scanning for weird / incorrect data (url instead of name)
+        Changing ID to be 0 indexed
+    """
 
+    for Entry in sorted_by_date:
+        #if URL, do nothing
+        if "://" in Entry['company']:
+            logging.info(f"Skipping {Entry['company']}")
+        else:
+            Entry['id'] = counter
+            counter += 1
+            final_list.append(Entry)
+
+    print(final_list)
     # Write response to file
     with open('cache.json', 'w', encoding='utf-8') as f:
         json.dump(final_list, f, ensure_ascii=False, indent=4)
+        
+    """
+    start = time.time()
+    end = time.time()
+    print(end - start)
+    """
--- job_tracker/job_tracker.js
+++ job_tracker/job_tracker.js
@@ -1,15 +1,20 @@
 $(document).ready(function() {
-    $.ajax({
-        'url': './functions/invoke.php',
-        'type': 'post',
-        'dataType': 'json',
-    })
-    .done( function (response) {
-        CreateJobsPage(response);
-    })
+    build_jobs()
 })
 
-function CreateJobsPage(data){
+function build_jobs(){
+    $(document).ready(function() {
+        $.ajax({
+            'url': './functions/invoke.php',
+            'type': 'post',
+            'dataType': 'json',
+        })
+        .done( function (response) {
+            build_jobs_resolved(response)
+        })
+    })
+}
+function build_jobs_resolved(data){
     var MainContent = document.getElementsByClassName("content")[0];
 
     for(var i = 0; i < data.length; i++){
@@ -17,6 +22,70 @@ function CreateJobsPage(data){
         //how_to_apply, id, location, title, type, url
         
         //get data by index and can access data from there 
-        console.log(data[i]["company"])
+        console.log(data[i]["company"]);
+
+        var TempDiv = document.createElement("div");
+        TempDiv.classList.add("job_posting")
+    
+        var TempHeader = document.createElement("h3");
+        TempHeader.innerHTML = data[i]["company"];
+        TempDiv.appendChild(TempHeader);
+
+        TempDiv.onclick = job_on_click;
+
+        MainContent.appendChild(TempDiv);
     }
+}
+
+
+function job_on_click(self){
+    self.cancelBubble = true;
+
+    var clicked_element = self.originalTarget;
+    var parent_div = self.originalTarget.parentElement;
+
+    var background_color = $(parent_div).css("background-color");
+
+    if(background_color=="rgb(0, 0, 0)"){
+        $(parent_div).css("width", "auto");
+        $(parent_div).css("background-color", "white");
+        clicked_element.style.color = 'black';
+
+        console.log($(parent_div).children())
+        $(parent_div).remove(1)
+    }
+    else{
+    $(parent_div).css("width", "50%");
+    $(parent_div).css("background-color", "black");
+    clicked_element.style.color = 'white';
+    
+    // call jobs page, pass to function and append to box 
+    $.ajax({
+        'url': './functions/invoke.php',
+        'type': 'post',
+        'dataType': 'json',
+    })
+    .done( function (response) {
+        job_on_click_resolved(response,self)
+    })
+    }
+}
+function job_on_click_resolved(data,this_event){
+    var clicked_name = this_event.originalTarget.innerHTML;
+    
+    var matched_json_object = ""
+    for(var i = 0; i < data.length; i++){
+        if(data[i].company==clicked_name){
+            matched_json_object=data[i];
+            break;
+        }
+    }
+    
+    var job_description_span = document.createElement("span");
+    job_description_span.classList.add("job_posting")
+    job_description_span.innerHTML = matched_json_object.description;
+
+    var parent_div = this_event.originalTarget.parentElement;
+    parent_div.appendChild(job_description_span)
+
 }
